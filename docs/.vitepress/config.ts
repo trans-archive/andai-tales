295f44f4485c7ff5f9bf3e2d22ec9de16f372d06
Lee <lee@mtftm.com>
2024-10-17 22:48:30 +0800

    config: 添加baseUrl

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -55,7 +55,7 @@ const sidebarOptions = [
 const themeConfig: ThemeContext = {
   siteTitle: "王安黛小说集",
   siteDescription: "曾用笔名苦樱/术后mtf/12年经验的百合推理作者",
-  // baseUrl: '/andai-tales/',
+  baseUrl: '/andai-tales/',
   org: 'Trans Archive!', // 这里用于设定版权区显示的组织名
   // siteLogo: '/logo.png',
   // SiteTitle值为false时，logo位置不显示标题。未定义SiteTitle时，显示标题。SiteTitle值为abcd时，显示abcd。
